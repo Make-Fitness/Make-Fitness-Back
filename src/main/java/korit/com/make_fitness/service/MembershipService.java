90fc405e6762965dfad72311567107213bab36e8
SHIRO <wogus2974@naver.com>
2025-04-03 17:45:09 +0900

    진짜 살려줄게

--- src/main/java/korit/com/make_fitness/service/MembershipService.java
+++ src/main/java/korit/com/make_fitness/service/MembershipService.java
@@ -6,10 +6,7 @@ import korit.com.make_fitness.entity.Customer;
 import korit.com.make_fitness.entity.Membership;
 import korit.com.make_fitness.repository.MembershipRepository;
 import korit.com.make_fitness.repository.UserRepository;
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
-=======
->>>>>>> 7a92de3d2b2a9a9fe8bdcd762b113a6ee33bc645
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,14 +16,9 @@ public class MembershipService {
     @Autowired
     private MembershipRepository membershipRepository;
 
-<<<<<<< HEAD
-=======
     public MembershipService(MembershipRepository membershipRepository, UserRepository userRepository) {
         this.membershipRepository = membershipRepository;
-        this.userRepository = userRepository;
     }
->>>>>>> 7a92de3d2b2a9a9fe8bdcd762b113a6ee33bc645
-
     @Transactional(rollbackFor = Exception.class)
     public Membership insertCustomer(ReqMembershipDto reqMembershipDto) {
         Membership membership = Membership.builder()
@@ -35,16 +27,8 @@ public class MembershipService {
                 .build();
 
         membershipRepository.save(membership);
-<<<<<<< HEAD
-=======
         System.out.println("✅ membership insert 성공");
 
-        int count = userRepository.updateRoleToCustomer(userId);
-        System.out.println("🧨 update count = " + count);
-
-        String newRole = userRepository.findByUserId(userId).get().getRoleName();
-        System.out.println("🧪 현재 role_name = " + newRole);
->>>>>>> 7a92de3d2b2a9a9fe8bdcd762b113a6ee33bc645
         return membership;
     }
 
