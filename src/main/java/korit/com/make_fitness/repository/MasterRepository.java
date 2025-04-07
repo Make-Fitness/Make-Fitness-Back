ca49a12adaaa1baa1fe59e4832d71fcba94cbed1
bsoupb <qotn0327@naver.com>
2025-04-07 14:33:11 +0900

    4/7(월) master 관련 페이지 설명 추가

--- src/main/java/korit/com/make_fitness/repository/MasterRepository.java
+++ src/main/java/korit/com/make_fitness/repository/MasterRepository.java
@@ -21,30 +21,37 @@ public class MasterRepository {
     @Autowired
     private UserMapper userMapper;
 
+    // 트레이너 삭제 기능
     public void deleteManager(String roleName, int userId) {
         masterMapper.deleteManager(userId, roleName);
     }
 
+    // userId로 조회하여 회원 권한 부여
     public void updateRoleByUserId(int userId, String roleName) {
         masterMapper.updateRoleName(userId, roleName);
     }
 
+    // 월별 관련 트레이너 조회
     public List<RespManagerDto> getManager(LocalDate classTime) {
         return masterMapper.findManager(classTime);
     }
 
+    // 이름으로 조회 가능한 회원가입자 조회
     public List<RespMemberListDto> searchMembers(String nickname) {
         return masterMapper.findByNickname(nickname);
     }
 
+    // 지정 날짜에 대한 매출 조회
     public List<RespSalesDto> searchSalesWithDates(ReqDateDto reqDateDto) {
         return masterMapper.findByStartAndEndDate(reqDateDto);
     }
 
+    // 월별 매출 조회
     public List<RespSalesDto> getSales(LocalDate startDate) {
         return masterMapper.findSales(startDate);
     }
 
+    // 멤버십 가입 시 roleName = ROLE_CUSTOMER
     public void updateRoleName(int userId) {
         userMapper.updateUserRoleToCustomer(userId);
     }
--- src/main/java/korit/com/make_fitness/service/MasterService.java
+++ src/main/java/korit/com/make_fitness/service/MasterService.java
@@ -18,29 +18,34 @@ public class MasterService {
     @Autowired
     private MasterRepository masterRepository;
 
-    //
+    // 트레이너 삭제
     @Transactional(rollbackFor = Exception.class)
     public void removeManager(String roleName, int userId) {
         masterRepository.deleteManager(roleName, userId);
     }
 
+    // 회원가입자 권한 변경
     @Transactional(rollbackFor = Exception.class)
     public void changeRoleName(int userId, String roleName) {
         masterRepository.updateRoleByUserId(userId, roleName);
     }
 
+    // 월별 관련 트레이너 목록 조회
     public List<RespManagerDto> getManager(LocalDate classTime) {
         return masterRepository.getManager(classTime);
     }
 
+    // 이름으로 검색 가능한 회원 조회
     public List<RespMemberListDto> searchMembers(String nickName) {
         return masterRepository.searchMembers(nickName);
     }
 
+    // 지정한 날짜에 대한 매출 조회
     public List<RespSalesDto> getSalesWithStartAndEndDate(ReqDateDto reqDateDto) {
         return masterRepository.searchSalesWithDates(reqDateDto);
     }
 
+    // 월별 매출 조회
     public List<RespSalesDto> getSales(LocalDate startDate) {
         return masterRepository.getSales(startDate);
     }
